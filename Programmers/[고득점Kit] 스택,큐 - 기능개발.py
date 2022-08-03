b125c0a28a26985572a03d437a7a50d5c76435b9
S.U.Kwon <kwonars@gmail.com>
2022-08-03 21:31:58 +0900

    [고득점Kit] 스택,큐 - 기능개발

---
+++ Programmers/[고득점Kit] 스택,큐 - 기능개발.py
@@ -0,0 +1,55 @@
+from collections import deque
+# double-ended queue
+
+# 벡터 합 계산
+def vecAdd(vecA, vecB):
+    vecSum = deque([])
+    if len(vecA) != len(vecB):
+        return "Error"
+    for idx, a in enumerate(vecA):
+        vecSum.append(vecB[idx] + a)
+    return vecSum
+
+# 첫 번째부터 몇번 째까지 100보다 큰 지 리턴
+def continuousOver100Num(progDec):
+    
+    if len(progDec) == 0:
+        return -1
+    
+    for idx, p in enumerate(progDec):
+        if p < 100:
+            return idx
+        else:
+            continue
+
+    return len(progDec)
+
+def solution(progresses, speeds):
+    
+    # 덱으로 바꿔줌
+    progressesDec = deque(progresses)
+    speedsDec = deque(speeds)
+    
+    end = False
+    answer = []
+    while(not end):
+
+        updatedProgressesDec = vecAdd(progressesDec, speedsDec) # 최초 프로그레스 진행
+        contNum = continuousOver100Num(updatedProgressesDec) # 최초 프로그레스 계산
+
+        if contNum > 0: # 맨 앞에거부터 끝난게 있으면
+            for i in range(contNum): # 그 수만큼 반복하여 popleft 시킴.
+                updatedProgressesDec.popleft()
+                speedsDec.popleft()
+            answer.append(contNum) # 답을 위한 return 값에 추가
+
+            progressesDec = updatedProgressesDec # 최초값 수정
+            
+            if len(updatedProgressesDec) == 0: # 모든 progresses를 다 popleft 시켰으면 끝냄
+                end = True
+
+        else: # 맨 앞부터 끝난거 없으면
+            progressesDec = updatedProgressesDec # 최초값 업데이트하여 반복
+            continue
+
+    return answer
